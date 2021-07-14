f05863d1345370b0cf466f9e7cdc655ecf36a7ec
vikeju <virginie.grolleau+github@protonmail.com>
2021-07-14 16:31:36 +0200

    TP module 5 (Mise en place doctrine et données)

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,22 +15,24 @@ class WishController extends AbstractController
     /**
      * @Route("", name="list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        // todo: afficher la liste des choses à faire
+        $wishes = $wishRepository->findBy([], ['dateCreated' => 'DESC']);
 
         return $this->render('wish/list.html.twig', [
-
+            "wishes" => $wishes
         ]);
     }
 
     /**
      * @Route("/details/{id}", name="details")
      */
-    public function details(int $id): Response
+    public function details(int $id, WishRepository $wishRepository): Response
     {
-        // todo: afficher les détails sur une idée de chose à faire
+        $wish = $wishRepository->find($id);
 
-        return $this->render('wish/details.html.twig');
+        return $this->render('wish/details.html.twig', [
+            "wish" => $wish
+        ]);
     }
 }
