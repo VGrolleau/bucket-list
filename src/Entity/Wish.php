9b24063ac76a4c9f19388ddea63f848984ece8fa
vikeju <virginie.grolleau+github@protonmail.com>
2021-07-19 18:28:43 +0200

    TP module 7 (Mise en relation tables category et wish)

--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -45,8 +45,8 @@ class Wish
      * @Assert\Length(
      *     min=1,
      *     max=50,
-     *     minMessage = "Le nom doit être d'au moins {{ limit }} caractères"
-     *     maxMessage="Le nom doit être inférieur à {{ limit }} caractères"
+     *     minMessage = "Le nom doit être d'au moins {{ limit }} caractères",
+     *     maxMessage = "Le nom doit être inférieur à {{ limit }} caractères"
      * )
      * @ORM\Column(type="string", length=50, nullable=true)
      */
@@ -61,6 +61,11 @@ class Wish
      * @ORM\Column(type="datetime")
      */
     private $dateCreated;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="wishes")
+     */
+    private $category;
 
     public function getId(): ?int
     {
@@ -123,6 +128,18 @@ class Wish
     public function setDateCreated(\DateTimeInterface $dateCreated): self
     {
         $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
--- src/Form/WishType.php
+++ src/Form/WishType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Wish;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,6 +25,10 @@ class WishType extends AbstractType
             ->add('author', null, [
                 'label' => 'Votre nom',
                 'required' => false
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
             ])
         ;
     }
